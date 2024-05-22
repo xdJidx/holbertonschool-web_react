0b99323a5c2dda161d4327be32106e98539a679b
xdJidx <bernardjerome59@gmail.com>
2024-05-22 10:34:42 +0200

    Create actions for the course list

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,13 @@
+// courseActionCreators.js
+
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+export const selectCourse = (index) => ({
+  type: SELECT_COURSE,
+  index
+});
+
+export const unSelectCourse = (index) => ({
+  type: UNSELECT_COURSE,
+  index
+});
